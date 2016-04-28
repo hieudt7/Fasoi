76b7289bd6b7f25baf0aff50ac4b2f14d91cb9a0
hieudt7 <hieudtx9@gmail.com>
2016-04-28 09:55:52 +0700

    commit project Fasoi

---
+++ scripts/Common.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    $('.login-drop').click(function () {
+        $(this).parents('li:first').addClass('open');
+    });
+    $('.dropdown-menu.login-drop').on('click.bs.dropdown', function (e) {
+        return false;
+    });
+    $('html').niceScroll({
+        cursorcolor: "#000",
+        cursorborder: "0px solid #fff",
+        railpadding: {
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0
+        },
+        cursorwidth: "10px",
+        cursorborderradius: "0px",
+        cursoropacitymin: 0.2,
+        cursoropacitymax: 0.8,
+        boxzoom: true,
+        horizrailenabled: false,
+        zindex: 9999
+    });
+});
